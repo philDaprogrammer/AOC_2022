a68fdffff996dc44b0820e028bf36b7dd979fcac
philDaprogrammer <75814801+philDaprogrammer@users.noreply.github.com>
2022-12-06 16:57:23 -0500

    Delete day1/target directory

--- day1/target/debug/build/memchr-de18f77c152bc3d8/build_script_build-de18f77c152bc3d8.d
+++
@@ -1,5 +0,0 @@
-/home/phil/IdeaProjects/rust/AOC/day1/target/debug/build/memchr-de18f77c152bc3d8/build_script_build-de18f77c152bc3d8: /home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
-
-/home/phil/IdeaProjects/rust/AOC/day1/target/debug/build/memchr-de18f77c152bc3d8/build_script_build-de18f77c152bc3d8.d: /home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs
-
-/home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.5.0/build.rs:
--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/phil/IdeaProjects/rust/AOC/day1/target/debug/day1: /home/phil/IdeaProjects/rust/AOC/day1/src/main.rs
