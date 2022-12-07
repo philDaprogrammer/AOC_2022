289f37b1ee494f6159ad48e704820a7b8c511778
philDaprogrammer <prstanto@buffalo.edu>
2022-12-06 22:36:36 -0500

    more concise, less readable

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -5,23 +5,20 @@ use std::collections::HashSet;
 fn solve(signal: String, size: usize) -> i32 {
     let mut  bits = signal.chars().collect::<Vec<char>>();
     let mut  header: LinkedList<char> =  LinkedList::new();
-
-    for _ in 0..size - 1 {
-        header.push_back(bits.remove(0));
-    }
-    
     let mut items: HashSet<char>;
 
     for i in 0..bits.len() {
         header.push_back(bits[i]);
-        items = HashSet::new();
 
+        if i < size - 1 { continue; }
+
+        items = HashSet::new();
         for bit in header.iter() {
             items.insert(*bit);
         }
 
         if items.len() == size {
-            return (i + size) as i32;
+            return (i + 1) as i32;
         }
 
         header.pop_front();
