53019bb6d54c4f6875d67bd35130e6f541b73f03
philDaprogrammer <prstanto@buffalo.edu>
2022-12-06 22:12:11 -0500

    complete

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,39 @@
+use std::fs;
+use std::collections::LinkedList;
+use std::collections::HashSet;
+
+fn solve(signal: String, size: usize) -> i32 {
+    let mut  bits = signal.chars().collect::<Vec<char>>();
+    let mut  header: LinkedList<char> =  LinkedList::new();
+
+    for _ in 0..size - 1 {
+        header.push_back(bits.remove(0));
+    }
+    
+    let mut items: HashSet<char>;
+
+    for i in 0..bits.len() {
+        header.push_back(bits[i]);
+        items = HashSet::new();
+
+        for bit in header.iter() {
+            items.insert(*bit);
+        }
+
+        if items.len() == size {
+            return (i + size) as i32;
+        }
+
+        header.pop_front();
+    }
+
+    -1
+}
+
 fn main() {
-    println!("Hello, world!");
+    let signal: String = fs::read_to_string("input.txt")
+        .expect("Error reading input");
+
+    println!("{}", solve(signal.clone(), 4));
+    println!("{}", solve(signal.clone(), 14));
 }
